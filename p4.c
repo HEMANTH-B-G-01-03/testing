22869eadb96a9be76a54ea3722a5ef87ab413405
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2024-11-08 23:37:04 +0530

    updates

--- p4.c
+++ p4.c
@@ -28,3 +28,65 @@ return 0;
 
 
 
+// TC
+// Id Test Case Description
+// Input Data Expected
+// Output
+// Actual
+// Output
+// Status
+// a b C
+// 1 Since a>10 the values of one
+// variable is greater
+// than the range
+// 11 3 2 Out of range Out of range Pass
+// 2 Since b>10 the values of one
+// variable is greater than the
+// range
+// 3 11 2 Out of range Out of range Pass
+// 3 Since c>10 the values of one
+// variable is greater
+// than 10
+// 3 2 11 Out of range Out of range Pass
+// 4 Since a<1the value of one
+// variable is lesser than
+// the range
+// 0 4 5 Out of range Out of range Pass
+// 5 Since b<1the value of one
+// variable is
+// lesser than the range
+// 4 0 5 Out of range Out of range Pass
+// 6 Since c<1the value of one
+// variable is lesser than
+// the range
+// 4 5 0 Out of range Out of range Pass
+
+
+// Since a, b>10 the values of
+// two variables is greater than
+// the range
+// 11 11 5 Out of range Out of range Pass
+// 2 Since b, c>10
+// the values of two variables
+//  is greater than the range
+// 5 11 11 Out of range Out of range Pass
+
+// Since a, c>10 the values of
+// two variables is greater than
+// range
+// 11 5 11 Out of range Out of range Pass
+// 4 Since a, b<0 the value of two
+// variablesis lesser than the range 0 0 5 Out of range Out of range Pass
+// 5 Since b, c<0the value of two
+// variables islesser than the
+// range
+// 5 0 0 Out of range Out of range Pass
+// 6 Since a, c<0 the value of
+// two variables is lesser than
+// the range
+// 0 5 0 Out of range Out of range Pass
+// 7 Since all the values greater than
+// 10 i.e greater than the range 11 11 11 Out of range Out of range Pass
+// 8 Since all the values lesser
+// than 0 i.e lesser than the range 0 0 0 Out of range Out of range P
+
