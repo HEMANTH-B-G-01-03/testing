0bddc49a7f386f3303ef609676f9aab327acd83d
HEMANTH <110120035+HEMANTH-B-G-01-03@users.noreply.github.com>
2024-12-27 09:18:18 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -11,24 +11,22 @@ class GoogleFormAutomation:
         time.sleep(2)
         
         name = "name"
-        name_input = self.driver.find_element(By.XPATH, '//input[@arialabelledby="i1 i4"]')
+        name_input = self.driver.find_element(By.XPATH, '//input[@aria-labelledby="i1 i4"]')
         name_input.send_keys(name)
         time.sleep(2)
-        
+
         usn = "4SF21IS000"
-        age_input = self.driver.find_element(By.XPATH, '//input[@arialabelledby="i6 i9"]')
+        age_input = self.driver.find_element(By.XPATH, '//input[@aria-labelledby="i6 i9"]')
         age_input.send_keys(usn)
         time.sleep(2)
-        
-        submit = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+
+        submit = self.driver.find_element(By.XPATH,
+            '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
         submit.click()
         time.sleep(2)
 
     def tearDown(self):
         self.driver.quit()
-
-
-
 
 if __name__ == "__main__":
     automation = GoogleFormAutomation()
